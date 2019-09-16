99d82b3cfa1150f1fae384d31d1df875d7501754
dyang <hidiyang@gmail.com>
2019-09-16 14:11:16 +1000

    Update multilabel_evaluation.py

--- multilabel_evaluation.py
+++ multilabel_evaluation.py
@@ -8,7 +8,7 @@ def multilabel_accuracy(true_labels, predicted_labels):
         predicted_label = predicted_labels[i]
         common_bits = 0.0
         for j in range(num_labels):
-            if (true_label[j] == predicted_label[j]) && true_label[i] > 0:
+            if (true_label[j] == predicted_label[j]) and true_label[i] > 0:
                 common_bits += 1.0
         accuracy_score += common_bits/(sum((true_label+predicted_label)>0))
     return (accuracy_score/batch_size)
@@ -21,7 +21,7 @@ def multilabel_precision(true_labels, predicted_labels):
         predicted_label = predicted_labels[i]
         common_bits = 0.0
         for j in range(num_labels):
-            if (true_label[j] == predicted_label[j]) && true_label[i] > 0:
+            if (true_label[j] == predicted_label[j]) and true_label[i] > 0:
                 common_bits += 1.0
         precision_score += common_bits/(sum(predicted_labels>0))
     return (precision_score/batch_size)
@@ -34,7 +34,7 @@ def multilabel_recall(true_labels, predicted_labels):
         predicted_label = predicted_labels[i]
         common_bits = 0.0
         for j in range(num_labels):
-            if (true_label[j] == predicted_label[j]) && true_label[i] > 0:
+            if (true_label[j] == predicted_label[j]) and true_label[i] > 0:
                 common_bits += 1.0
         recall_score += common_bits/(sum(true_labels>0))
     return (recall_score/batch_size)
